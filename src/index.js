384449e7e6944b87a8c102de4c1933efc595ec66
Quinten Clause <quintenclause@hotmail.com>
2024-03-16 15:04:01 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,19 @@ import entities from './entities.js';
 import {components} from 'mini-web-game-components';
 import {system} from 'mini-web-game-system';
 
+try {
+    if (process.env.NODE_ENV === 'production') {
+        if ('serviceWorker' in navigator) {
+            window.addEventListener('load', e => {
+                navigator.serviceWorker.register(
+                    new URL('./sw.js', import.meta.url),
+                    {type: 'module'}
+                );
+            });
+        }
+    }
+} catch (err) {}
+
 system.game(entities, components);
 
 export default {};
